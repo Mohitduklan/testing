950e6d1243634ad5b139c146a572b5926f41d3af
a <a>
2025-10-02 10:25:30 +0530

    Refactor variable names for consistency and clarity; replace 'data' with 'df' throughout the code.

--- dask_program1.py
+++ dask_program1.py
@@ -1,49 +1,56 @@
+
 import dask.dataframe as dd
 import time
-data = dd.read_csv("./data/large_file.csv")
-print(data.head())
-print(data.columns)
+df = dd.read_csv("./data/large_file.csv")
+print(df.head())
+print(df.columns)
+
 
 # Compute to do eval
-print(data.shape[0].compute()) 
+print(df.shape[0].compute())
+
 
 # # Filtering
-filtered_data = data[data["value"]>600]
-print(filtered_data.head())
+filtered_df = df[df["value"] > 600]
+print(filtered_df.head())
+
 
 # Value Count
 t = time.time()
-val_count = data["category"].value_counts().compute()
+val_count = df["category"].value_counts().compute()
 print(time.time() - t)
 print(val_count)
 
 t = time.time()
-val_count = data["category"].value_counts(split_every=4).compute()
+val_count = df["category"].value_counts(split_every=4).compute()
 print(time.time() - t)
 print(val_count)
+
 
 
 # Time stamps
 t = time.time()
-print(data["timestamp"].dtype) # It is string
-print(time.time()-t)
-data["timestamp"] = dd.to_datetime(data["timestamp"])
-print(time.time()-t)
-data = data.set_index(data["timestamp"])
-print(time.time()-t)
-mean = data["value"]
+print(df["timestamp"].dtype) # It is string
+print(time.time() - t)
+df["timestamp"] = dd.to_datetime(df["timestamp"])
+print(time.time() - t)
+df = df.set_index(df["timestamp"])
+print(time.time() - t)
+mean = df["value"]
 print(mean.head(10))
 
 
 
+
 # Time stamps
-print(data["timestamp"].dtype) # It is string
-data["timestamp"] = dd.to_datetime(data["timestamp"])
-data = data.set_index(data["timestamp"])
-data = data["value"].resample("1H").mean()
-print(data.head(10))
+print(df["timestamp"].dtype) # It is string
+df["timestamp"] = dd.to_datetime(df["timestamp"])
+df = df.set_index(df["timestamp"])
+df = df["value"].resample("1H").mean()
+print(df.head(10))
+
 
 
 # Transform
-print(data.groupby("category")["value"].agg(lambda x: x - x.mean()))
-print(data.groupby("category")["value"].transform(lambda x: x - x.mean()))
+print(df.groupby("category")["value"].agg(lambda x: x - x.mean()))
+print(df.groupby("category")["value"].transform(lambda x: x - x.mean()))
